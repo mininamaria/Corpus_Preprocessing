6127e827a8866cc42011e28a714f8ca4e371d797
mminina <43503936+mminina@users.noreply.github.com>
2022-10-19 17:22:47 +0300

    Added a draft: read and lemmatize files

---
+++ main.py
@@ -0,0 +1,37 @@
+# Import Module
+import os
+from pymystem3 import Mystem
+
+#text = "Красивая мама красиво мыла раму"
+#m = Mystem()
+#lemmas = m.lemmatize(text)
+#print(''.join(lemmas))
+
+
+# Folder Path
+path = "C:/Users/Maria/Desktop/temporary"
+
+# Change the directory
+os.chdir(path)
+m = Mystem()
+
+# Create text File
+
+
+# Read text File
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        lemmas = m.lemmatize(f.read())
+        with open('edited.txt', 'w') as f_to_w:
+            f_to_w.write(''.join(lemmas))
+        print("Mission complete! New file is ready!", file_path)
+
+
+# iterate through all file
+for file in os.listdir():
+    # Check whether file is in text format or not
+    if file.endswith(".txt"):
+        file_path = f"{path}\{file}"
+
+        # call read text file function
+        read_text_file(file_path)
